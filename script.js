a511817b32d97d72f25cf255a14488702d6dbb6b
Hieronimus03 <hidayatbw983@gmail.com>
2024-12-17 16:14:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+// Mengenali Tipe Data
+let partOne = "Bagian 1: Mengenali Tipe Data";
+
+console.log(partOne);
+let myName ='Hieronimus';
+
+
+
+let age = 21;
+let isStudent = true;
+let emptyValue = null;
+let notDefined;
+
+console.log('Name: '+ myName);
+console.log('Age: '+ age);
+console.log("isStudent: "+ isStudent);
+console.log(`emptyValue: ${emptyValue}`);
+console.log('notDefined: '+ notDefined );
+
+
+// Menggunakan Arithmetic Operators
+
+let partTwo = "Bagian 2: Menggunakan Arithmetic Operators";
+
+console.log(partTwo);
+
+let a = 10;
+let b = 3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// Menggunakan Assignment Operators
+
+let partThree = "Bagian 3: Menggunakan Assignment Operators";
+
+console.log(partThree);
+
+let x1 =5;
+
+x1 += 10; 
+console.log(x1);
+
+
+let x2 =5;
+x2 -= 5; 
+console.log(x2);
+
+let x3 =5;
+x3 *= 3; 
+console.log(x3);
+
+let x4 =5;
+x4 /= 4; 
+console.log(x4);
+
+let x5 =5;
+x5 %= 2; 
+console.log(x5);
+
+// String Operators
+let partFour = "Bagian 4: String Operators";
+
+console.log(partFour);
+
+let firstName ="Hieronimus ";
+let midName = "Hidayat ";
+let lastName ="Bawamenewi,";
+
+let fullName =firstName +  midName +   lastName;
+
+fullName += " Selamat Datang ";
+console.log(fullName);
+
+// Menggabungkan Semua Konsep
+
+let partFive = "Bagian 5: Menggabungkan Semua Konsep";
+
+console.log(partFive);
+
+let myFullName ='Hieronimus Hidayat Bawamenewi';
+let myAge = 11;
+let iAmAStudent = true;
+let myUniversitas = "UPI YPTK Padang"
+let mySemester = 7;
+
+console.log(typeof myFullName);
+console.log(typeof myAge);
+console.log(typeof iAmAStudent);
+console.log(typeof myUniversitas);
+console.log(typeof mySemester);
+myAge += 10;
+
+let setence =
+
+'Nama lengkap saya adalah '+
+ myFullName +
+ ', sekarang saya berusia '+
+ myAge +
+ ' tahun, dan status saya adalah '+
+ iAmAStudent +
+' sebagai mahasiswa semester ' +
+mySemester +
+' di '+
+myUniversitas +
+'.'
+;    
+
+
+console.log(setence);
+
+
+
